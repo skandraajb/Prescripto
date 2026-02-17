44d26241e2c8c9ca74e6c18fcd0629761c929366
skandraajb <skandraajb@gmail.com>
2026-02-17 20:15:40 +0530

    update for deploy

--- frontend/src/api/apiConfig.js
+++ frontend/src/api/apiConfig.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api', // Your backend base URL
+  baseURL: 'https://prescripto-g2ot.onrender.com/api', // Your backend base URL
   withCredentials: true,                 // IMPORTANT: send cookies with requests for auth
 });
 
