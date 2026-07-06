e9a656558a9420c67666d12e476b21fd401a70ac
skandraajb <skandraajb@gmail.com>
2026-07-06 22:10:21 +0530

    Debug trial

--- frontend/src/api/apiConfig.js
+++ frontend/src/api/apiConfig.js
@@ -1,5 +1,7 @@
 import axios from 'axios';
 
+console.log("API BASE URL:", process.env.REACT_APP_API_URL);
+
 const api = axios.create({
   baseURL: process.env.REACT_APP_API_URL || 'http://localhost:5000/api',
   withCredentials: true,
